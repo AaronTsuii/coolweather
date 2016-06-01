b1d03602ad9cb043501d04d73411c3210cf4b453
Summer_ann <cuijiawen0514@163.com>
2016-06-01 11:43:14 +0800

    加入显示天气信息的功能

--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -1,5 +1,15 @@
 package com.coolweather.app.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -13,9 +23,9 @@ public class Utility {
 	//解析和处理服务器返回的省级数据
 	public synchronized static boolean handleProvinceResponse(CoolWeatherDB coolWeatherDB,String response) {
 		if(!TextUtils.isEmpty(response)) {
-			Log.d("开始解析",response);
+			//Log.d("开始解析",response);
 			String [] allProvinces = response.split(",");
-			Log.d("开始解析",""+ allProvinces.length);
+			//Log.d("开始解析",""+ allProvinces.length);
 			if(allProvinces != null && allProvinces.length > 0) {
 				for(String p : allProvinces) {
 					String [] array = p.split("\\|");
@@ -58,7 +68,7 @@ public class Utility {
 			if(allCounties != null && allCounties.length > 0) {
 				for(String c : allCounties) {
 					String [] array = c.split("\\|");
-					Log.d("解析单个县",""+ array[1]);
+					//Log.d("解析单个县",""+ array[1]);
 					County county = new County();
 					county.setCountyCode(array[0]);
 					county.setCountyName(array[1]);
@@ -69,5 +79,44 @@ public class Utility {
 			}
 		}
 		return false;
+	}
+	
+	//解析服务器返回的JSON数据，并将解析出的数据存储到本地
+	public static void handleWeatherResponse(Context context ,String response) {
+		try{
+			Log.d("1", "3");
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp = weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+			Log.d("1", "4");
+			saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,publishTime);
+			
+		}catch(JSONException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//将服务器返回的所有天气信息存储到SharedPreferences文件中
+	private static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+		Log.d("1", "5");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日",Locale.CHINA);
+		SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code",weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		Log.d("1", "6");
+		editor.commit();
 	}
 }
